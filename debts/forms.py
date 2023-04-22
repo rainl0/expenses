bf996de29bda0e6ea08c063fb13e53caba26ce54
rl <rainalar.laurimaa@real.edu.ee>
2023-04-22 15:15:17 +0300

    finished project

--- debts/forms.py
+++ debts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from debts.models import Money
+from django.contrib.auth import get_user_model
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -18,10 +19,24 @@ class NewUserForm(UserCreationForm):
     #     return user
 
 class PaymentForm(forms.ModelForm):
-    payee = forms.CharField(required=True, max_length=100)
-    sum = forms.IntegerField(required=True)
-    note = forms.CharField(required=False, max_length=240)
+    # payer = forms.CharField()
+    # payee = forms.CharField(required=True, max_length=100)
+    # sum = forms.IntegerField(required=True)
+    # note = forms.CharField(required=False, max_length=240)
+    # date = forms.DateTimeField()
 
     class Meta:
         model = Money
         fields = ['payee', 'sum', 'note']
+        exclude = ['payer', 'date']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        payee = cleaned_data.get("payee")
+        sum = cleaned_data.get("sum")
+
+        if payee and sum:
+            users = []
+            [users.append(name[0]) for name in get_user_model().objects.values_list('username')]
+            if payee not in users:
+                raise forms.ValidationError(("Invalid name"), code="invalid")
--- debts/views.py
+++ debts/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.forms import ValidationError
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
@@ -13,51 +14,51 @@ def main(request):
     model = Money
     field_names = [f.name for f in model._meta.get_fields()]
     field_names.remove('id')
-    print(field_names)
     data = [[getattr(ins, name) for name in field_names]
             for ins in model.objects.prefetch_related().all()]
     return render(request, 'main.html', {'field_names': field_names, 'data': data})
 
 def register_request(request):
-	if request.method == "post":
+	form = NewUserForm()
+	if request.method == "POST":
 		form = NewUserForm(request.POST)
 		if form.is_valid():
 			user = form.save()
-			user.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("/")
+			return redirect("account/")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
 	message = messages.get_messages(request)
 	return render(request=request, template_name="registration/register.html", context={"form":form, "messages": message})
 
 @login_required
 def accountView(request):
-	if request.method == "post":
+	form = PaymentForm()
+	model = Money
+
+	if request.method == "POST":
 		form = PaymentForm(request.POST)
 		if form.is_valid():
-			users = get_user_model().objects.values_list('username')
-			if form.payee not in users:
-				messages.error(request, "Please enter a valid username (it's case sensitive!)")
 			payment = form.save(commit=False)
 			payment.payer = request.user.username
 			payment.date = datetime.datetime.now()
 			payment.save()
 			messages.success(request, "Success!")
-	
-	form = PaymentForm()
-	model = Money
+			return redirect("/")
 	
     #find amount of money owed, received and net
 	i = 0
 	got = 0
 	owed = 0
-	for payer, payee in list(zip(model.objects.values_list('payer'), model.objects.values_list('payee'))):
+	payers = []
+	payees = []
+	[payers.append(payer[0]) for payer in model.objects.values_list('payer')]
+	[payees.append(payee[0]) for payee in model.objects.values_list('payee')]
+	for payer, payee in list(zip(payers, payees)):
 		if payer == request.user.username:
-			owed += model.objects.values_list('sum')[i]
+			owed += model.objects.values_list('sum')[i][0]
 		if payee == request.user.username:
-			got += model.objects.values_list('sum')[i]
+			got += model.objects.values_list('sum')[i][0]
 		i += 1
 	net = got - owed
 	
